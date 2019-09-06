ccb0333b61461ff1c5ba04a164a0f0705c59d798
Duc Pham <ducpmdtc.dtc@gmail.com>
2019-09-06 18:24:38 +0700

    Duc

--- js/main.js
+++ js/main.js
@@ -4,4 +4,28 @@
 	$('.grid').masonry({
         itemSelector: '.grid-item',
     });
+
+    $(".carousel-description").html($(".carousel--mobileview .carousel-indicators li").html());
+    $('.carousel--mobileview').on('slid.bs.carousel', function () {
+        $('.carousel-description').html($(".carousel--mobileview .carousel-indicators .active").html());
+    });
+
+    /* secondary indicators for carousel */
+    $('.carousel').on('slid.bs.carousel', function() {
+      $(".carousel-indicators2 li").removeClass("active");
+      indicators = $(".carousel-indicators li.active").data("slide-to");
+      a = $(".carousel-indicators2").find("[data-slide-to='" + indicators + "']").addClass("active");
+      //console.log(indicators);
+    })
+    // on click on indicators2;
+    $('.carousel-indicators2 li').on('click', function() {
+        newindicator = $(this).data("slide-to");
+        $(".carousel-indicators li").removeClass("active");
+        $(".carousel-indicators2 li").removeClass("active");
+        $(".carousel-indicators2").find("[data-slide-to='" + newindicator + "']").addClass("active");
+        $(".carousel-indicators2").find("[data-slide-to='" + newindicator + "']").addClass("active");
+        //console.log(newindicator);
+    });
+    /************************/
+
 })(jQuery); // End of use strict
